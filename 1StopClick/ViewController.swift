4b78522ea192d205d9038516d486dfc01cd917ad
Wisnu Anggoro <wisnu@anggoro.net>
2019-09-13 13:48:38 +0700

    Initial Commit

---
+++ 1StopClick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1StopClick
+//
+//  Created by MTMAC18 on 13/09/19.
+//  Copyright © 2019 Wibi Herlangga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
