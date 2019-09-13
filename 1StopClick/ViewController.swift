c3e84c68fbff2ec5cfb2d42c531a09619d51f9d6
Wisnu Anggoro <wisnu@anggoro.net>
2019-09-13 15:07:49 +0700

    Adding View Will Appear

--- 1StopClick/ViewController.swift
+++ 1StopClick/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.\
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
 
